f53b7fd0ffc2f0cf5c2968bf1727a8d0f3b37699
Nate Merritt <nate.merritt@biblionexus.org>
2024-11-11 12:50:00 -0500

    Use dependency injection.

--- src/BibleWell.App/App.axaml.cs
+++ src/BibleWell.App/App.axaml.cs
@@ -4,8 +4,11 @@ using Avalonia.Data.Core.Plugins;
 using Avalonia.Markup.Xaml;
 using BibleWell.App.ViewModels;
 using BibleWell.App.Views;
+using Microsoft.Extensions.DependencyInjection;
 
 namespace BibleWell.App;
+
+// this is the Avalonia equivalent of a Program.cs file
 public partial class App : Application
 {
     public override void Initialize()
@@ -15,24 +18,37 @@ public partial class App : Application
 
     public override void OnFrameworkInitializationCompleted()
     {
+        // configure dependency injection
+        var services = new ServiceCollection();
+        ConfigureServices(services);
+        var serviceProvider = services.BuildServiceProvider();
+
+        // configure Avalonia app main window
+        var mainViewModel = serviceProvider.GetRequiredService<MainViewModel>();
         if (ApplicationLifetime is IClassicDesktopStyleApplicationLifetime desktop)
         {
             // Line below is needed to remove Avalonia data validation.
             // Without this line you will get duplicate validations from both Avalonia and CT
             BindingPlugins.DataValidators.RemoveAt(0);
+
             desktop.MainWindow = new MainWindow
             {
-                DataContext = new MainViewModel()
+                DataContext = mainViewModel,
             };
         }
         else if (ApplicationLifetime is ISingleViewApplicationLifetime singleViewPlatform)
         {
             singleViewPlatform.MainView = new MainView
             {
-                DataContext = new MainViewModel()
+                DataContext = mainViewModel,
             };
         }
 
         base.OnFrameworkInitializationCompleted();
+    }
+
+    internal static void ConfigureServices(IServiceCollection services)
+    {
+        services.AddTransient<MainViewModel>();
     }
 }
