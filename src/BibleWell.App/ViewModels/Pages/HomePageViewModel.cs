5acfb6bc21164446ac94a48d3c754d028c1a1385
Nate Merritt <nate.merritt@biblionexus.org>
2025-04-11 11:05:13 -0400

    Add theme switcher for light/dark mode support.

--- src/BibleWell.App/ViewModels/Pages/HomePageViewModel.cs
+++ src/BibleWell.App/ViewModels/Pages/HomePageViewModel.cs
@@ -1,5 +1,16 @@
-﻿namespace BibleWell.App.ViewModels.Pages;
+﻿using Avalonia;
+using Avalonia.Styling;
+using CommunityToolkit.Mvvm.Input;
+
+namespace BibleWell.App.ViewModels.Pages;
 
 public sealed partial class HomePageViewModel : PageViewModelBase
 {
+    [RelayCommand]
+    public void ChangeTheme()
+    {
+        Application.Current!.RequestedThemeVariant = Application.Current!.ActualThemeVariant == ThemeVariant.Dark
+            ? ThemeVariant.Light
+            : ThemeVariant.Dark;
+    }
 }
