6741f58b0ba87c0bf45b2fa0b4365c9795f1af2c
kaseywright <kasey.wright@biblionexus.org>
2025-04-22 12:36:28 -0400

    Add simple Sqlite to dev page

--- src/BibleWell.App/ViewModels/Pages/DevPageViewModel.cs
+++ src/BibleWell.App/ViewModels/Pages/DevPageViewModel.cs
@@ -1,13 +1,34 @@
+using BibleWell.Aquifer;
+using CommunityToolkit.Mvvm.ComponentModel;
+using CommunityToolkit.Mvvm.Input;
+
 namespace BibleWell.App.ViewModels.Pages;
 
 /// <summary>
 /// Design-time view model for use with the <see cref="Views.Pages.DevPageView"/>.
 /// </summary>
-public sealed class DesignDevPageViewModel() : DevPageViewModel();
+public sealed class DesignDevPageViewModel() : DevPageViewModel(null!);
 
 /// <summary>
 /// View model for use with the <see cref="Views.Pages.DevPageView"/>.
 /// </summary>
-public partial class DevPageViewModel : PageViewModelBase
+public partial class DevPageViewModel(IReadWriteAquiferService _sqliteAquiferService) : PageViewModelBase
 {
+    [ObservableProperty]
+    private string _resourceContentHtml = "<p>Click the button to view content.</p>";
+
+    [RelayCommand]
+    public async Task LoadResourceContentAsync()
+    {
+        try
+        {
+            var resourceContent = await _sqliteAquiferService.GetResourceContentAsync(1);
+            ResourceContentHtml = resourceContent?.Content ?? "resource not found";
+        }
+        catch (Exception ex)
+        {
+            Console.WriteLine(ex);
+            throw;
+        }
+    }
 }
