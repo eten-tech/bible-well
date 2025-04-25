00579e03ec68007c2a25727d97d640d3cde9749c
Nate Merritt <nate.merritt@biblionexus.org>
2025-04-25 13:05:15 -0400

    Implement router tests.

--- tests/BibleWell.App.UnitTests/RouterTests.cs
+++ tests/BibleWell.App.UnitTests/RouterTests.cs
@@ -1,10 +1,63 @@
-﻿namespace BibleWell.App.UnitTests;
+﻿using BibleWell.App.ViewModels;
+using BibleWell.App.ViewModels.Pages;
+using CommunityToolkit.Mvvm.DependencyInjection;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace BibleWell.App.UnitTests;
 
 public sealed class RouterTests
 {
     [Fact]
-    public void BackwardsNavigation_ShouldCorrectlyNavigate()
+    public void Router_Navigation_ShouldCorrectlyNavigate()
     {
+        // These are the only services needed for the router to work.
+        // We don't care about the services being injected into the view models for this test so inject null.
+        var services = new ServiceCollection()
+            .AddTransient<HomePageViewModel>(_ => new HomePageViewModel(null!))
+            .AddTransient<BiblePageViewModel>(_ => new BiblePageViewModel())
+            .AddTransient<GuidePageViewModel>(_ => new GuidePageViewModel())
+            .AddTransient<ResourcesPageViewModel>(_ => new ResourcesPageViewModel(null!));
 
+        Ioc.Default.ConfigureServices(services.BuildServiceProvider());
+
+        var router = new Router<ViewModelBase>();
+        router.Current.Should().BeNull();
+        router.CanGoBack.Should().BeFalse();
+        router.CanGoForward.Should().BeFalse();
+
+        router.GoTo<HomePageViewModel>();
+        router.Current.Should().BeOfType<HomePageViewModel>();
+        router.CanGoBack.Should().BeFalse();
+        router.CanGoForward.Should().BeFalse();
+
+        router.GoTo<BiblePageViewModel>();
+        router.Current.Should().BeOfType<BiblePageViewModel>();
+        router.CanGoBack.Should().BeTrue();
+        router.CanGoForward.Should().BeFalse();
+
+        router.GoTo<GuidePageViewModel>();
+        router.Current.Should().BeOfType<GuidePageViewModel>();
+        router.CanGoBack.Should().BeTrue();
+        router.CanGoForward.Should().BeFalse();
+
+        router.Back();
+        router.Current.Should().BeOfType<BiblePageViewModel>();
+        router.CanGoBack.Should().BeTrue();
+        router.CanGoForward.Should().BeTrue();
+
+        router.Back();
+        router.Current.Should().BeOfType<HomePageViewModel>();
+        router.CanGoBack.Should().BeFalse();
+        router.CanGoForward.Should().BeTrue();
+
+        router.Forward();
+        router.Current.Should().BeOfType<BiblePageViewModel>();
+        router.CanGoBack.Should().BeTrue();
+        router.CanGoForward.Should().BeTrue();
+
+        router.GoTo<ResourcesPageViewModel>();
+        router.Current.Should().BeOfType<ResourcesPageViewModel>();
+        router.CanGoBack.Should().BeTrue();
+        router.CanGoForward.Should().BeFalse();
     }
 }
