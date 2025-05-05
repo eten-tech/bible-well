042c3d544758666aa141480dfbde8667f7042c43
Nate Merritt <nate.merritt@biblionexus.org>
2025-05-05 18:03:43 -0400

    Refresh app main view on selected language change.
    Also, save selected language in user preferences.

--- src/BibleWell.App/ViewModels/Pages/LanguagesPageViewModel.cs
+++ src/BibleWell.App/ViewModels/Pages/LanguagesPageViewModel.cs
@@ -1,15 +1,41 @@
-﻿using CommunityToolkit.Mvvm.Input;
+﻿using System.Collections.ObjectModel;
+using System.Globalization;
+using Avalonia;
+using BibleWell.Preferences;
+using CommunityToolkit.Mvvm.ComponentModel;
+using CommunityToolkit.Mvvm.Input;
 
 namespace BibleWell.App.ViewModels.Pages;
 
 /// <summary>
 /// View model for use with the <see cref="Views.Pages.LanguagesPageView"/>.
 /// </summary>
-public partial class LanguagesPageViewModel(Router _router) : PageViewModelBase
+public partial class LanguagesPageViewModel(Router _router, IUserPreferencesService _userPreferencesService) : PageViewModelBase
 {
+    [ObservableProperty]
+    private CultureInfo? _selectedCultureInfo;
+
+    public static ObservableCollection<CultureInfo> SupportedCultureInfos { get; } =
+    [
+        new("en-US"),
+        new("es-ES"),
+    ];
+
     [RelayCommand]
     public void Close()
     {
         _router.Back();
+    }
+
+    partial void OnSelectedCultureInfoChanged(CultureInfo? value)
+    {
+        if (value == null)
+        {
+            return;
+        }
+
+        _userPreferencesService.Set(PreferenceKeys.Language, value.Name);
+        Thread.CurrentThread.CurrentUICulture = value;
+        ((App)Application.Current!).ReloadMainView<HomePageViewModel>();
     }
 }
