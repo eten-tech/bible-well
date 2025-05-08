1d74a7f7d06898c02a60e44baa7d12591a8d9e78
Nate Merritt <nate.merritt@biblionexus.org>
2025-05-08 12:21:23 -0400

    Fix Android App when not in Development environment.

--- src/BibleWell.App.Android/AndroidApp.cs
+++ src/BibleWell.App.Android/AndroidApp.cs
@@ -13,12 +13,15 @@ public sealed class AndroidApp : App
 {
     protected override void ConfigurePlatform(ConfigurationBuilder configurationBuilder, string environment)
     {
-        // Ideally we'd dispose of this stream, but we can't because it's used by the configuration builder in the Build() call.
-        // The GC will dispose of it eventually.
-        var environmentConfigurationSettingsFileStream = GetAppSettingsFileStream(
-            Assembly.GetExecutingAssembly(),
-            $"appsettings.{environment}.json");
-        configurationBuilder.AddJsonStream(environmentConfigurationSettingsFileStream);
+        if (environment == nameof(AppEnvironment.Development))
+        {
+            // Ideally we'd dispose of this stream, but we can't because it's used by the configuration builder in the Build() call.
+            // The GC will dispose of it eventually.
+            var environmentConfigurationSettingsFileStream = GetAppSettingsFileStream(
+                Assembly.GetExecutingAssembly(),
+                $"appsettings.{environment}.json");
+            configurationBuilder.AddJsonStream(environmentConfigurationSettingsFileStream);
+        }
     }
 
     protected override void RegisterPlatformServices(IServiceCollection services)
